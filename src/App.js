fb0a66a14b038510d62608aa4fdae75a90003e6d
aaru-06 <arumugampillai0610@gmail.com>
2021-01-16 23:28:43 +0530

    changed cat image to answer script image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SectionComponent from "./Components/SectionComponent";
 import 'bootstrap/dist/css/bootstrap.css';
 import test from "./Data/getSubmissionTest.json"
+import image from "./Data/2_2.jpg";
 
 class App extends Component {
   
@@ -33,12 +34,12 @@ class App extends Component {
   }
 
   render(){
-    
+    // "https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697"
     return (
       <div className="App">
           <SectionComponent width="15%" heading="questions" data={this.state.test} handleMarkState ={this.handleMarkState} />
           <SectionComponent width="15%" heading="segments" data={this.state.segments} />
-          <SectionComponent width="50%" heading="answer scripts" data={"https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697"}/>
+          <SectionComponent width="50%" heading="answer scripts" data={image}/>
           <SectionComponent width="20%" heading="marks allocation" data="dummy"
                             isSubQuestionVisible={this.state.isSubQuestionVisible}
                             currQno={this.state.currQno}
